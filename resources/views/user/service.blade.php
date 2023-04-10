46220bd4f7fb708a1e4b1ce768697ef2e5d1bd8d
RifkiBachtiar <rifkibachtiar11@gmail.com>
2023-04-11 05:42:07 +0700

    update service rifki

--- resources/views/user/service.blade.php
+++ resources/views/user/service.blade.php
@@ -16,17 +16,13 @@
 </div>
 <div class="container-xxl py-5">
   <div class="container">
-    <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-      <h5 class="section-title ff-secondary text-center text-primary fw-normal">Our Services</h5>
-      <h1 class="mb-5">Explore Our Services</h1>
-    </div>
     <div class="row g-4">
       <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
         <div class="service-item rounded pt-3">
           <div class="p-4">
             <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
             <h5>Master Chefs</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            <p>Ini merupakan form card karyawan yang bekerja di restoran kami</p>
           </div>
         </div>
       </div>
@@ -34,8 +30,8 @@
         <div class="service-item rounded pt-3">
           <div class="p-4">
             <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
-            <h5>Quality Food</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            <h5>Kualitas Makanan</h5>
+            <p>Ini merupakan informasi berisi kualitas dari makanan yang kami sediakan</p>
           </div>
         </div>
       </div>
@@ -44,7 +40,7 @@
           <div class="p-4">
             <i class="fa fa-3x fa-cart-plus text-primary mb-4"></i>
             <h5>Online Order</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            <p>Ini merupakan informasi cara untuk melakukan order online untuk customer</p>
           </div>
         </div>
       </div>
@@ -53,43 +49,7 @@
           <div class="p-4">
             <i class="fa fa-3x fa-headset text-primary mb-4"></i>
             <h5>24/7 Service</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
-            <h5>Master Chefs</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-utensils text-primary mb-4"></i>
-            <h5>Quality Food</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-cart-plus text-primary mb-4"></i>
-            <h5>Online Order</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-        <div class="service-item rounded pt-3">
-          <div class="p-4">
-            <i class="fa fa-3x fa-headset text-primary mb-4"></i>
-            <h5>24/7 Service</h5>
-            <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            <p>Ini merupakan forum pengaduan kritik & saran kami untuk customer</p>
           </div>
         </div>
       </div>
